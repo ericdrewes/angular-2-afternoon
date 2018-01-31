b4c4ccf084e23d4d5633c797741b1e1f657b43f5
ericdrewes <eric.d.drewes@gmail.com>
2018-01-31 15:51:23 -0600

    finished afternoon project

--- app/mainSrvc.js
+++ app/mainSrvc.js
@@ -1,3 +1,10 @@
-angular.module('devmtnTravel').service('mainSrvc', function() {
+angular.module('devmtnTravel').service('mainSrvc', function($http) {
+    this.getTravelInfo = function() {
+       http.get('https://practiceapi.devmountain.com/api/travel/travel-info')
+    }
+
+    this.getPackageInfo = function() {
+        http.get('https://practiceapi.devmountain.com/api/travel/package-info')
+    }
   
 });
